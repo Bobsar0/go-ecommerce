0826f138c0337cbe7ba7cf58bad9635c0ba5bfd0
Steve Onyeneke <bobsarglobal@gmail.com>
2022-12-22 14:28:35 +0000

    refactor json rep of models

--- models/models.go
+++ models/models.go
@@ -3,7 +3,6 @@ package models
 import (
 	"time"
 
-	"go-mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
@@ -12,17 +11,17 @@ type User struct {
 	ID             primitive.ObjectID `json:"_id" bson:"_id"`
 	FirstName      *string            `json:"firstName" validate:"required,min=2,max=30"`
 	LastName       *string            `json:"lastName" validate:"required,min=2,max=30"`
-	Password       *string            `json:"password" validate:"required,min=6`
-	Email          *string            `json:"email" validate:"required`
-	Phone          *string            `json:"phone" validate:"required`
+	Password       *string            `json:"password" validate:"required,min=6"`
+	Email          *string            `json:"email" validate:"required"`
+	Phone          *string            `json:"phone" validate:"required"`
 	Token          *string            `json:"token"`
 	RefreshToken   *string            `json:"refreshToken"`
 	CreatedAt      time.Time          `json:"createdAt"`
 	UpdatedAt      time.Time          `json:"updatedAt"`
-	UserCart       []UserProduct      `json:"userCart" bson:"userCart`
+	UserCart       []UserProduct      `json:"userCart" bson:"userCart"`
 	AddressDetails []Address          `json:"addressDetails" bson:"addressDetails"`
 	OrderStatus    []Order            `json:"orders" bson:"orders"`
-	UserId         string             `json:"userId`
+	UserId         string             `json:"userId"`
 }
 
 type Product struct {
@@ -52,7 +51,7 @@ type Address struct {
 type Order struct {
 	OrderId       primitive.ObjectID `bson:"_id"`
 	OrderCart     []UserProduct      `json:"orderList" bson:"orderList"`
-	OrderedAt     *time.Time         `json:"orderedAt" bson:"orderedAt`
+	OrderedAt     time.Time          `json:"orderedAt" bson:"orderedAt"`
 	Price         int                `json:"totalPrice" bson:"totalPrice"`
 	Discount      *int               `json:"discount" bson:"discount"`
 	PaymentMethod Payment            `json:"paymentMethod" bson:"paymentMethod"`
