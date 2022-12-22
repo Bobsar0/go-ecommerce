828d6302fee6439fceb76faa782068c423deb4f6
Steve Onyeneke <bobsarglobal@gmail.com>
2022-12-22 14:19:25 +0000

    implement cart controller handlers

--- controllers/cart.go
+++ controllers/cart.go
@@ -1,22 +1,209 @@
 package controllers
 
-import "github.com/gin-gonic/gin"
+import (
+	"context"
+	"errors"
+	"go-ecommerce/database"
+	"go-ecommerce/models"
+	"log"
+	"net/http"
+	"time"
 
-func AddToCart() gin.HandlerFunc {
-	
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+type Application struct {
+	prodCollection *mongo.Collection
+	userCollection *mongo.Collection
 }
 
-func RemoveItem() gin.HandlerFunc {
-	
+func NewApplication(prodCollection, userCollection *mongo.Collection) *Application {
+	return &Application{prodCollection: prodCollection, userCollection: userCollection}
 }
 
-func GetItemFromCat() gin.HandlerFunc {
-	
-}
-func BuyFromCart()  gin.HandlerFunc{
-	
+func (app *Application) AddToCart() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		// you need user who is adding and product to be added.
+		productQueryId := c.Query("id")
+		if productQueryId == "" {
+			log.Println("Product ID is empty")
+			_ = c.AbortWithError(http.StatusBadRequest, errors.New("Product ID is empty"))
+			return
+		}
+
+		userQueryId := c.Query("userId")
+		if userQueryId == "" {
+			log.Println("User ID is empty")
+			_ = c.AbortWithError(http.StatusBadRequest, errors.New("User ID is empty"))
+			return
+		}
+
+		// productId, err := primitive.ObjectIDFromHex(productQueryId)
+
+		// if err != nil {
+		// 	log.Println(err)
+		// 	c.AbortWithStatus(http.StatusInternalServerError)
+		// 	return
+		// }
+
+		var ctx, cancel = context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancel()
+
+		err := database.AddToCart(ctx, app.prodCollection, app.userCollection, productQueryId)
+		if err != nil {
+			c.IndentedJSON(http.StatusInternalServerError, err)
+		}
+		c.IndentedJSON(http.StatusOK, "Successfully added to cart")
+	}
 }
 
-func InstantBuy() gin.HandlerFunc {
-	
+func (app *Application) RemoveItem() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		// you need user who is adding and product to be added.
+		productQueryId := c.Query("id")
+		if productQueryId == "" {
+			log.Println("Product ID is empty")
+			_ = c.AbortWithError(http.StatusBadRequest, errors.New("Product ID is empty"))
+			return
+		}
+
+		userQueryId := c.Query("userId")
+		if userQueryId == "" {
+			log.Println("User ID is empty")
+			_ = c.AbortWithError(http.StatusBadRequest, errors.New("User ID is empty"))
+			return
+		}
+
+		productId, err := primitive.ObjectIDFromHex(productQueryId)
+
+		if err != nil {
+			log.Println(err)
+			c.AbortWithStatus(http.StatusInternalServerError)
+			return
+		}
+
+		var ctx, cancel = context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancel()
+
+		err = database.RemoveItemFromCart(ctx, app.prodCollection, app.userCollection, productId, userQueryId)
+		if err != nil {
+			c.IndentedJSON(http.StatusInternalServerError, err)
+		}
+		c.IndentedJSON(http.StatusOK, "Successfully removed product from cart")
+	}
+}
+
+func (app *Application) GetItemFromCart() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		id := c.Query("id")
+
+		if id == "" {
+			log.Println("id is empty")
+
+			c.Header("Content-Type", "application/json")
+			c.JSON(http.StatusNotFound, gin.H{"error": "Invalid id"})
+			c.Abort()
+			return
+		}
+
+		userId, _ := primitive.ObjectIDFromHex(id)
+
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+		defer cancel()
+
+		var filledCart models.User
+
+		if err := UserCollection.FindOne(ctx, bson.D{primitive.E{Key: "_id", Value: userId}}); err != nil {
+			log.Println(err)
+			c.IndentedJSON(500, "Not found")
+			return
+		}
+
+		// create aggregation query
+		// match finds the particular user with user id and its data
+		filterMatch := bson.D{{Key: "$match", Value: bson.D{primitive.E{Key: "_id", Value: userId}}}}
+		// unwind stage will unwind the userCart data from a closed array to something that can be processed in go
+		unwind := bson.D{{Key: "$unwind", Value: bson.D{primitive.E{Key: "path", Value: "$userCart"}}}}
+
+		// grouping stage groups all the values with the help of id and find the total price
+		grouping := bson.D{{Key: "$group", Value: bson.D{primitive.E{Key: "$_id", Value: "$_id"}, {Key: "$sum", Value: "$userCart.price"}}}}
+		pointCursor, err := UserCollection.Aggregate(ctx, mongo.Pipeline{filterMatch, unwind, grouping})
+
+		if err != nil {
+			log.Println(err)
+		}
+
+		var listing []bson.M
+		// convert the values in pointCursor to listing
+		if err = pointCursor.All(ctx, &listing); err != nil {
+			log.Println(err)
+			c.AbortWithStatus(http.StatusInternalServerError)
+		}
+
+		for _, json := range listing {
+			c.IndentedJSON(200, json["total"])
+			c.IndentedJSON(200, filledCart.UserCart)
+		}
+		ctx.Done()
+	}
+}
+
+func (app *Application) BuyFromCart() gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+		userQueryId := c.Query("userId")
+		if userQueryId == "" {
+			log.Println("User ID is empty")
+			_ = c.AbortWithError(http.StatusBadRequest, errors.New("User ID is empty"))
+			return
+		}
+
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+		defer cancel()
+
+		err := database.BuyItemFromCart(ctx, app.userCollection, userQueryId)
+		if err != nil {
+			c.IndentedJSON(http.StatusInternalServerError, err)
+		}
+		c.IndentedJSON(http.StatusOK, "Successfully placed the order")
+	}
+}
+
+func (app *Application) InstantBuy() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		// you need user who is adding and product to be added.
+		productQueryId := c.Query("id")
+		if productQueryId == "" {
+			log.Println("Product ID is empty")
+			_ = c.AbortWithError(http.StatusBadRequest, errors.New("Product ID is empty"))
+			return
+		}
+
+		userQueryId := c.Query("userId")
+		if userQueryId == "" {
+			log.Println("User ID is empty")
+			_ = c.AbortWithError(http.StatusBadRequest, errors.New("User ID is empty"))
+			return
+		}
+
+		productId, err := primitive.ObjectIDFromHex(productQueryId)
+
+		if err != nil {
+			log.Println(err)
+			c.AbortWithStatus(http.StatusInternalServerError)
+			return
+		}
+
+		var ctx, cancel = context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancel()
+
+		err = database.InstantBuy(ctx, app.prodCollection, app.userCollection, productId, userQueryId)
+		if err != nil {
+			c.IndentedJSON(http.StatusInternalServerError, err)
+		}
+		c.IndentedJSON(http.StatusOK, "Successfully placed the order")
+	}
 }
--- controllers/controller.go
+++ controllers/controller.go
@@ -1,31 +1,231 @@
 package controllers
 
-import "github.com/gin-gonic/gin"
+import (
+	"context"
+	"fmt"
+	"go-ecommerce/database"
+	"go-ecommerce/models"
+	generate "go-ecommerce/token"
 
-func HashPassword(password string)  string{
+	"log"
+	"net/http"
+	"time"
 
+	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"golang.org/x/crypto/bcrypt"
+)
+
+var (
+	UserCollection    *mongo.Collection = database.UserData(database.Client, "Users")
+	ProductCollection *mongo.Collection = database.ProductData(database.Client, "Products")
+	Validate                            = validator.New()
+)
+
+func HashPassword(password string) string {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
+	if err != nil {
+		log.Panic(err)
+	}
+	return string(bytes)
 }
 
-func VerifyPassword(userPassword, givenPassword string)  (bool, string){
-	
+func VerifyPassword(userPassword, givenPassword string) (bool, string) {
+	err := bcrypt.CompareHashAndPassword([]byte(givenPassword), []byte(userPassword))
+	valid := true
+	msg := ""
+
+	if err != nil {
+		msg = "Login or Password is incorrect"
+		valid = false
+	}
+	return valid, msg
 }
 
 func Signup() gin.HandlerFunc {
-	
+	return func(c *gin.Context) {
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+		defer cancel()
+
+		var user models.User
+		if err := c.BindJSON(&user); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		//check if email address exists
+		count, err := UserCollection.CountDocuments(ctx, bson.M{"email": user.Email})
+
+		if err != nil {
+			log.Panic(err)
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err})
+		}
+
+		if count > 0 {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "User already exists"})
+		}
+
+		//check if phone exists
+		count, err = UserCollection.CountDocuments(ctx, bson.M{"phone": user.Phone})
+		if count > 0 {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "This phone number is already in use"})
+		}
+
+		password := HashPassword(*user.Password)
+		user.Password = &password
+
+		user.CreatedAt, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+		user.UpdatedAt, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+		user.ID = primitive.NewObjectID()
+		user.UserId = user.ID.Hex()
+
+		token, refreshToken, _ := generate.TokenGenerator(*user.Email, *user.FirstName, *user.LastName, user.UserId)
+		user.Token = &token
+		user.RefreshToken = &refreshToken
+		user.UserCart = make([]models.UserProduct, 0)
+		user.AddressDetails = make([]models.Address, 0)
+		user.OrderStatus = make([]models.Order, 0)
+
+		if _, insertErr := UserCollection.InsertOne(ctx, user); insertErr != nil {
+			fmt.Println("Error in creating user: ", insertErr)
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "The user did not get created"})
+
+		}
+	}
 }
 
-func Login() gin.HandlerFunc{
+func Login() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+		defer cancel()
 
+		var user, foundUser models.User
+		// put the user details captured in the request context [c] in user model
+		if err := c.BindJSON(&user); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		if err := UserCollection.FindOne(ctx, bson.M{"email": user.Email}).Decode(&foundUser); err != nil {
+			c.JSON(http.StatusNotFound, gin.H{"error": err.Error()})
+			return
+		}
+
+		//verify password
+		passwordIsValid, msg := VerifyPassword(*user.Password, *foundUser.Password)
+		defer cancel()
+
+		if !passwordIsValid {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": msg})
+			fmt.Println("Error verifying password: ", msg)
+			return
+
+		}
+		token, refreshToken, _ := generate.TokenGenerator(*foundUser.Email, *foundUser.FirstName, *foundUser.LastName, foundUser.UserId)
+		defer cancel() //WHY DEFER CANCEL ALL THE TIME??
+
+		// update user tokens
+		generate.UpdateAllTokens(token, refreshToken, foundUser.UserId)
+		c.JSON(http.StatusFound, foundUser)
+	}
 }
 
-func ProductViewerAdmin() gin.HandlerFunc  {
-	
+func ProductViewerAdmin() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+		defer cancel()
+		var products models.Product
+		defer cancel()
+
+		if err := c.BindJSON(&products); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+		products.ProductId = primitive.NewObjectID()
+		if _, err := ProductCollection.InsertOne(ctx, products); err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "not inserted"})
+			return
+		}
+		defer cancel()
+		c.JSON(http.StatusOK, "successfully added")
+	}
 }
 
-func SearchProduct()  gin.HandlerFunc{
-	
+func SearchProduct() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		var productList []models.Product
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+		defer cancel()
+
+		// pass empty braces to return all collections
+		cursor, err := ProductCollection.Find(ctx, bson.D{{}})
+
+		if err != nil {
+			c.IndentedJSON(http.StatusInternalServerError, "Soemtiong went wrong, please try again later")
+			return
+		}
+
+		err = cursor.All(ctx, &productList)
+		if err != nil {
+			log.Println(err)
+			c.AbortWithStatus(http.StatusInternalServerError)
+			return
+		}
+		defer cursor.Close(ctx)
+
+		if err = cursor.Err(); err != nil {
+			log.Println(err)
+			c.IndentedJSON(http.StatusBadRequest, "Invalid")
+			return
+		}
+		defer cancel()
+		c.IndentedJSON(http.StatusOK, productList)
+	}
 }
 
 func SearchProductByQuery() gin.HandlerFunc {
-	
+	return func(c *gin.Context) {
+		var searchProducts []models.Product
+		queryParam := c.Query("name")
+
+		// check if the queryParam is emopty
+		if queryParam == "" {
+			log.Println("query is empty")
+
+			c.Header("Content-Type", "application/json")
+			c.JSON(http.StatusNotFound, gin.H{"error": "Invalid search index"})
+			c.Abort()
+			return
+		}
+
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+		defer cancel()
+
+		searchQueryDb, err := ProductCollection.Find(ctx, bson.M{"productName": bson.M{"$regex": queryParam}})
+
+		if err != nil {
+			c.IndentedJSON(404, "Something went wrong while fetching product")
+			return
+		}
+
+		err = searchQueryDb.All(ctx, &searchProducts)
+		if err != nil {
+			log.Println(err)
+			c.IndentedJSON(400, "Invalid")
+			return
+		}
+		defer searchQueryDb.Close(ctx)
+
+		if err := searchQueryDb.Err(); err != nil {
+			log.Println(err)
+			c.IndentedJSON(400, "Invalid request")
+			return
+		}
+
+		defer cancel()
+		c.IndentedJSON(200, searchProducts)
+	}
 }
